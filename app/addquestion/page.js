ada4f1a7e819b48f2177dafa3f6fbe5a45ba9e25
Muhammad Ahmed <ahmadginideveloper@gmail.com>
2023-08-22 03:31:50 -0700

    Design SigIn, SignUP, AddQuestions and Quiz page

--- app/addquestion/page.js
+++ app/addquestion/page.js
@@ -1,41 +1,41 @@
 
 export default function page() {
   return (
-    <div class="container mx-auto p-8">
-    <h1 class="text-3xl font-bold mb-4 text-center">Add Quiz Question</h1>
+    <div className="container mx-auto p-8">
+    <h1 className="text-3xl font-bold mb-4 text-center">Add Quiz Question</h1>
 
-    <div class="bg-gray-100  p-10 rounded-lg shadow-xl">
+    <div className="bg-gray-100  p-10 rounded-lg shadow-xl">
       <form>
-        <div class="mb-4">
-          <label for="question" class="block text-lg font-semibold mb-2">Question:</label>
-          <textarea id="question" name="question" rows="3" class="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"></textarea>
+        <div className="mb-4">
+          <label for="question" className="block text-lg font-semibold mb-2">Question:</label>
+          <textarea id="question" name="question" rows="3" className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"></textarea>
         </div>
 
-        <div class="mb-4">
-          <label for="option1" class="block font-semibold mb-2">Option 1:</label>
-          <input type="text" id="option1" name="option1" class="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"/>
+        <div className="mb-4">
+          <label for="option1" className="block font-semibold mb-2">Option 1:</label>
+          <input type="text" id="option1" name="option1" className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"/>
         </div>
 
-        <div class="mb-4">
-          <label for="option2" class="block font-semibold mb-2">Option 2:</label>
-          <input type="text" id="option2" name="option2" class="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"/>
+        <div className="mb-4">
+          <label for="option2" className="block font-semibold mb-2">Option 2:</label>
+          <input type="text" id="option2" name="option2" className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"/>
         </div>
 
-        <div class="mb-4">
-          <label for="option3" class="block font-semibold mb-2">Option 3:</label>
-          <input type="text" id="option3" name="option3" class="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"/>
+        <div className="mb-4">
+          <label for="option3" className="block font-semibold mb-2">Option 3:</label>
+          <input type="text" id="option3" name="option3" className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"/>
         </div>
 
-        <div class="mb-4">
-          <label for="correctOption" class="block font-semibold mb-2">Correct Option:</label>
-          <select id="correctOption" name="correctOption" class="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500">
+        <div className="mb-4">
+          <label for="correctOption" className="block font-semibold mb-2">Correct Option:</label>
+          <select id="correctOption" name="correctOption" className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500">
             <option value="option1">Option 1</option>
             <option value="option2">Option 2</option>
             <option value="option3">Option 3</option>
           </select>
         </div>
 
-        <button type="submit" class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded">Add Question</button>
+        <button type="submit" className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded">Add Question</button>
       </form>
     </div>
 
--- app/quiz/page.js
+++ app/quiz/page.js
@@ -1,27 +1,27 @@
 
 export default function page() {
   return (
-    <div class="container mx-auto p-8 mt-10">
+    <div className="container mx-auto p-8 mt-10">
     
-    <div class="bg-gray-100   p-11  rounded-lg shadow-xl">
-      <h2 class="text-2xl font-semibold mb-2">Question 1: What is the capital of France?</h2>
-      <div class="space-y-2 text-lg">
-        <label class="flex items-center">
-          <input type="radio" name="q1" value="paris" class="mr-2"/>
+    <div className="bg-gray-100   p-11  rounded-lg shadow-xl">
+      <h2 className="text-2xl font-semibold mb-2">Question 1: What is the capital of France?</h2>
+      <div className="space-y-2 text-lg">
+        <label className="flex items-center">
+          <input type="radio" name="q1" value="paris" className="mr-2"/>
           Paris
         </label>
-        <label class="flex items-center">
-          <input type="radio" name="q1" value="london" class="mr-2"/>
+        <label className="flex items-center">
+          <input type="radio" name="q1" value="london" className="mr-2"/>
           London
         </label>
-        <label class="flex items-center">
-          <input type="radio" name="q1" value="berlin" class="mr-2"/>
+        <label className="flex items-center">
+          <input type="radio" name="q1" value="berlin" className="mr-2"/>
           Berlin
         </label>
       </div>
     </div>
-     <div class="mt-4 text-end mx-6">
-      <button class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded">Next</button>
+     <div className="mt-4 text-end mx-6">
+      <button className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded">Next</button>
     </div>
   </div>
   )
